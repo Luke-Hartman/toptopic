bdba070f2af4094cd6e3525f1b3d37497d091042
Luke Hartman <lukehartman123@gmail.com>
2023-07-02 08:17:12 -0400

    Add Firebase.

---
+++ src/lib/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUpUe020GuVG7E7qw9L1wVO9PeCCFmkOw",
+  authDomain: "toptopic-1033d.firebaseapp.com",
+  projectId: "toptopic-1033d",
+  storageBucket: "toptopic-1033d.appspot.com",
+  messagingSenderId: "290977021616",
+  appId: "1:290977021616:web:77611b003c93a7713f0575",
+  measurementId: "G-0DZXZS0LXS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
--- src/routes/club/[inviteCode]/+page.server.js
+++ src/routes/club/[inviteCode]/+page.server.js
@@ -1,9 +1,16 @@
 import { get } from 'svelte/store';
 import { clubs } from '$lib/stores';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '$lib/config/firebase';
 
-export function load({ params }) {
+export async function load({ params }) {
 	console.log('params', params);
 	console.log('get(clubs)', get(clubs));
+
+	const querySnapshot = await getDocs(collection(db, 'clubs'));
+	querySnapshot.forEach((doc) => {
+		console.log(`${doc.id} => ${doc.data()}`);
+	});
 	return {
 		inviteCode: params.inviteCode,
 		clubName: get(clubs)[params.inviteCode]
